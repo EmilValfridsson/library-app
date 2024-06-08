b9cc5760df1f7f57a191f5f531927c550c1748ba
Emil <emil.walfridsson@icloud.com>
2024-06-08 11:11:09 +0200

    created login and register

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,3 +1,72 @@
+import { useForm } from "react-hook-form";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { Navigate, useNavigate } from "react-router-dom";
+import { auth } from "../services";
+
+const schema = z.object({
+  username: z
+    .string()
+    .min(1, { message: "Username is required." })
+    .min(3, { message: "Username is too short." }),
+  password: z.string().min(1, { message: "Password is required." }),
+});
+type FormData = z.infer<typeof schema>;
 export default function LoginPage() {
-  return <h1>LoginPage</h1>;
+  const {
+    setError,
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
+  const navigate = useNavigate();
+  const user = auth.getCurrentUser();
+  console.log(user);
+  async function onSubmit(data: FormData) {
+    try {
+      await auth.login(data);
+      console.log(data);
+      navigate("");
+    } catch (error: any) {
+      setError("username", { message: error.response.data });
+    }
+  }
+
+  if (user) return <Navigate to="/" />;
+
+  return (
+    <div className="h-screen grid place-items-center place-content-center">
+      <h1 className="mb-4 text-center font-bold text-3xl">Login</h1>
+      <div className="p-10 shadow rounded-3xl ">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="mb-3">
+            <label className="form-label">Username</label>
+            <input
+              {...register("username")}
+              className="input input-bordered w-full max-w-xs"
+            />
+            {errors.username && (
+              <p className="text-error">{errors.username.message}</p>
+            )}
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Password</label>
+            <input
+              {...register("password")}
+              className="input input-bordered w-full max-w-3xl"
+            />
+            {errors.password && (
+              <p className="text-error">{errors.password.message}</p>
+            )}
+          </div>
+          <div className="grid place-items-center mt-4">
+            <button className="btn btn-primary" disabled={!isValid}>
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,3 +1,81 @@
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { user } from "../services";
+import { useNavigate } from "react-router-dom";
+
+const schema = z.object({
+  name: z.string(),
+  username: z
+    .string()
+    .min(1, { message: "Username is required." })
+    .email({ message: "Invalid email." }),
+  password: z
+    .string()
+    .min(1, { message: "Password is required." })
+    .min(5, { message: "Password is too short." }),
+});
+
+type FormData = z.infer<typeof schema>;
+
 export default function RegisterPage() {
-  return <h1>RegisterPage</h1>;
+  const {
+    setError,
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<FormData>({ resolver: zodResolver(schema), mode: "onChange" });
+  const navigate = useNavigate();
+  async function onSubmit(data: FormData) {
+    try {
+      console.log("submitted", data);
+      await user.register(data);
+      navigate("");
+    } catch (error: any) {
+      if (error.response.status === 400) {
+        setError("username", { message: error.response.data });
+      }
+    }
+  }
+  return (
+    <div className="h-screen grid place-items-center place-content-center">
+      <h1 className="mb-4 text-center font-bold text-3xl">Register Page</h1>
+      <div className="p-10 shadow rounded-3xl ">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="mb-3">
+            <label className="form-label">Name</label>
+            <input
+              {...register("name")}
+              className="input input-bordered w-full max-w-xs"
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Username</label>
+            <input
+              {...register("username")}
+              className="input input-bordered w-full max-w-xs"
+            />
+            {errors.username && (
+              <p className="text-danger">{errors.username.message}</p>
+            )}
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Password</label>
+            <input
+              {...register("password")}
+              className="input input-bordered w-full max-w-xs"
+            />
+            {errors.password && (
+              <p className="text-danger">{errors.password.message}</p>
+            )}
+          </div>
+          <div className="d-grid justify-content-center mt-4">
+            <button className="btn btn-primary" disabled={!isValid}>
+              Register
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
